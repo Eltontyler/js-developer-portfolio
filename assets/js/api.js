402202c7d1cce36b8c304671b2fbd6ed683900eb
Elton <eltonconker@gmail.com>
2026-01-02 21:02:21 -0300

    atualizações

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,4 @@
-
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
+  const response = await fetch('./data/profile.json', { cache: 'no-store' })
+  return await response.json()
 }
